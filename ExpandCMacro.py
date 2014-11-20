4d0369cfedc249ca335cd0592f2cf884f9f01014
Pierre-Louis <pierre-louis.caron-auger@noviflow.com>
2014-11-20 11:53:15 -0500

    Initial commit - functional.

---
+++ ExpandCMacro.py
@@ -0,0 +1,77 @@
+#
+#
+
+import sublime, sublime_plugin, os, ntpath, subprocess, codecs, re
+
+class ExpandCppMacroCommand(sublime_plugin.TextCommand):
+
+	def __init__(self, view):
+		self.load_settings()
+		self.view = view
+		self.regex = re.compile("\s*#define\s+\w+([\s\S]+)")
+
+	def load_settings(self):
+		# Variable $project_base_path in settings will be replaced by sublime's project path
+		settings = sublime.load_settings("ExpandCMacro.sublime-settings")
+		project_path=""
+		if sublime.active_window().project_data() is not None:
+			project_path = (sublime.active_window().project_data().get("folders")[0].get("path"))
+
+		self.tmp_file_path    = settings.get("tmp_file_path")
+		self.default_encoding = settings.get("default_encoding")
+		self.include_dirs     = settings.get("include_dirs")
+		self.compiler         = settings.get("compiler")
+		self.output           = settings.get("output")
+		self.other_flags      = settings.get("other_flags")
+		for i in range(0, len(self.include_dirs)):
+			self.include_dirs[i] = re.sub("(\$project_base_path)", project_path, self.include_dirs[i])
+
+	def run(self, edit, output_dst):
+		view = self.view
+		# Find exact Line:Column position of cursor for clang
+		pos = view.sel()[0].begin()
+		body = view.substr(sublime.Region(0, view.size()))
+
+		line_pos = body[:pos].count('\n')
+		file_lines = body.split('\n')
+
+		# Create dummy file which we will send to preprocessor
+		tmp_file_header = "#include \""+ntpath.basename(self.view.file_name())+"\"\n"
+		line_to_expand = file_lines[line_pos]
+		regex_result = re.findall(self.regex, line_to_expand)
+		print(regex_result)
+		if len(regex_result) > 0:
+			line_to_expand = regex_result[0]
+
+		with open(self.tmp_file_path, "w", encoding='UTF-8') as tmp_file:
+			tmp_file.write(tmp_file_header + line_to_expand + "\n")
+	
+		# By default I assumed clang, but gcc should work as well
+		clang_bin = self.compiler
+		clang_flags = " -E "
+		clang_target = self.tmp_file_path
+		clang_includes=" -I ."
+		for dir in self.include_dirs:
+			clang_includes += " -I " + dir
+
+		# Execute clang command, exit 0 to suppress error from check_output()
+		clang_cmd = clang_bin + " " + clang_flags + clang_target + clang_includes + " " + self.other_flags
+		output = subprocess.check_output(clang_cmd+" | tail -n 2;exit 0", shell=True)
+		view.window().create_output_panel(name="test")
+		output_text = ''.join(map(chr,output))
+		output_lines = output_text.split('\n')
+
+		# Retrieve result and show it to user
+		result = output_lines[-2]
+
+		if output_dst == "file":
+			results = view.window().new_file()
+			results.set_scratch(True)
+			results.set_name("Macro")
+			point = results.insert(edit,0,result)
+			results.set_syntax_file(view.settings().get('syntax'))
+		elif output_dst == "panel":
+			view.window().show_quick_panel([result], None)
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-ExpandCMacro
-============
+#ClangAutoComplete
 
 Sublime Text 3 plugin that expands C/C++ macros.
+
+##Example
+
+```
+#include <stdio.h>
+
+
+#define PRINT_2(MSG, ARGS...) printf(MSG, ##ARGS);
+#define PRINT(MSG,ARGS...) PRINT_2(MSG,##ARGS)
+#define MACRO(X)                                              \
+{                                                             \
+    if (1)                                                    \
+        PRINT("this is a test %d\n", X)                       \
+    else                                                      \
+        PRINT("---- %s -----\n", "this is another test")      \
+}
+
+int main()
+{
+    MACRO(25)
+    return 0;
+}
+```
+
+Executing the command on the line `MACRO(25)` will result in following output
+
+`{ if (1) printf("this is a test %d\n", 25); else printf("---- %s -----\n", "this is another test"); }`
+
+
+##Installation
+
+1. Install "clang" or "gcc".
+2. Clone this repository and put it in your Sublime's packages directory.
+3. Edit the settings to your needs.
+4. Default key bindings are: "ctrl-alt-x" to put result into new file, "ctrl-alt-z" to put result in quick panel.
+5. To modify those bindings, simply edit "DDefault (OSX).sublime-keymap" or its linux counterpart.
+
+##Settings
+
+ - include_dirs: List of directories where relevant header files are located
+ - compiler: Use gcc or clang to get result
+ - other_flags: You can define compile time macros here with "-D DEFINE_SOEMTHING"
+ 
